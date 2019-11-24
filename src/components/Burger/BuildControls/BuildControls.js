6fb441ea0d51da5a747ec40c9faef4dfa78daf21
Ваше Имя <you@example.com>
2019-11-24 16:03:33 +0300

    Added style  BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Сатат', type: 'salad'},
+    { label: 'Бекон', type: 'bacon'},
+    { label: 'Сыр', type: 'cheese'},
+    { label: 'Мясо', type: 'meat'},
+];
+
 const buildControls = (props) => (
-    <div></div>
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
+    </div>
     )
 
 export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
+import BuilControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component {
         return (
           <Aux>
               <Burger ingredients={this.state.ingredients}/>
-              <div>Build Control</div>
+              <BuilControls/>
           </Aux>
         );
     }
