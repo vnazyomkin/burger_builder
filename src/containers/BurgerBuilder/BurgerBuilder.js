8684f5f22b31f4492682e41ec1aed8b854052ef1
Ваше Имя <you@example.com>
2019-11-24 11:52:07 +0300

    Starting

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxilliary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+          <Aux>
+              <div>Burger</div>
+              <div>Build Control</div>
+          </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
