2857dd6b93cfb2f0d4c49326e2ba3e9b81ce654d
Ваше Имя <you@example.com>
2019-12-11 20:03:38 +0300

    Added navigation's interface

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Меню</div>
         <Logo/>
         <nav>
-            <ul>...</ul>
+            <NavigationItems/>
         </nav>
     </header>
 );
