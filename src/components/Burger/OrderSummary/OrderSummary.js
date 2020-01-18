d02f37e4f853a1f767a4a4986bb880c5ee058768
Ваше Имя <you@example.com>
2020-01-18 19:15:57 +0300

    Finish basic version

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Auxilliary';
 import dictRu from "../../../dictionary/ingredientsNames";
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    console.log('sg');
-    const ingredientSummary = Object.keys(props.ingredients).map( el => {
+class  OrderSummary extends Component {
+    // componentWillUpdate() {
+    //     console.log('[OrderSummary] WillUpdate');
+    // }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map( el => {
+            return (
+                <li key={el}>
+                    <span style={{textTransform: 'capitalize'}}>{dictRu[el]}</span>: {this.props.ingredients[el]}
+            </li> );
+            } );
         return (
-            <li key={el}>
-                <span style={{textTransform: 'capitalize'}}>{dictRu[el]}</span>: {props.ingredients[el]}
-            </li>
-        )});
-    return (
-        <Aux>
-            <h3>Ваш заказ</h3>
-            <p>Супер-бургер со следующими ингредиентами:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Общая стоимость: {props.totalPrice} руб.</strong> </p>
-            <p>Продолжить оформление заказа?</p>
-            <Button
-                btnType='Success'
-                clicked={props.modalClose}>ОТМЕНА</Button>
-            <Button
-                btnType='Danger'
-                clicked={props.purchaseContinue}>ПРОДОЛЖИТЬ</Button>
-        </Aux>
-    );
+            <Aux>
+                <h3>Ваш заказ</h3>
+                <p>Супер-бургер со следующими ингредиентами:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Общая стоимость: {this.props.totalPrice} руб.</strong> </p>
+                <p>Продолжить оформление заказа?</p>
+                <Button
+                    btnType='Success'
+                    clicked={this.props.modalClose}>ОТМЕНА</Button>
+                <Button
+                    btnType='Danger'
+                    clicked={this.props.purchaseContinue}>ПРОДОЛЖИТЬ</Button>
+            </Aux>
+        );
+    }
+
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,22 +2,29 @@ import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true,
+        showSideDrawer: false,
     }
 
-    sideDrawerClosedHandler = () => {
+     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerOpenedHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
     }
 
     render() {
         return (
             <Aux>
-                <Toolbar/>
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerOpenedHandler}/>
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}/>
