8b1427027a0f4da1f9d84d697768c71c0ecdad2b
Val <val@debian>
2020-03-15 14:36:12 +0300

    Added Orgers container

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+const order = () => (
+    <div className={classes.Order}>
+        <p>Ингредиены: Салат (1)</p>
+        <p>Цена: <strong>200 руб.</strong></p>
+    </div>
+);
+
+
+export default order;
