507c0ac5391fe75755c34dc52935ae0741c88ca9
Val <val@debian>
2020-03-15 16:47:10 +0300

    Finished routing

---
+++ src/components/UI/LOV/Controls.js
@@ -0,0 +1,8 @@
+const controls = [
+    { label: 'Сатат', type: 'salad'},
+    { label: 'Бекон', type: 'bacon'},
+    { label: 'Сыр', type: 'cheese'},
+    { label: 'Мясо', type: 'meat'},
+];
+
+export default controls;
