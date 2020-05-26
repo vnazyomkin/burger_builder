a7cd4506399193b77a0d1b9dae4cf20c632182c3
Val <val@debian>
2020-05-26 11:01:39 +0300

    Added Redux Devtools

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 import {createStore} from 'redux'
 import reducer from './store/reducer';
 import {Provider} from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-let store = createStore(reducer);
+let store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
